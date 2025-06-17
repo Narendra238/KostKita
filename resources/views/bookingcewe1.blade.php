e16cbe54cd68cb2478b9e0f39703c4eb059070fd
Narendra238 <masnarendro123@gmail.com>
2025-06-17 21:23:57 +0700

    Tambah Aktifkan Navbar By Shofa Aurellia

--- resources/views/bookingcewe1.blade.php
+++ resources/views/bookingcewe1.blade.php
@@ -46,7 +46,7 @@
         <div class="container-fluid bg-dark px-0">
             <div class="row gx-0">
                 <div class="col-lg-3 bg-dark d-none d-lg-block">
-                    <a href="index.html" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                    <a href="/" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
                         <h1 class="m-0 text-primary">Kost KITA</h1>
                     </a>
                 </div>
@@ -73,7 +73,7 @@
                         </div>
                     </div> -->
                     <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
-                        <a href="index.html" class="navbar-brand d-block d-lg-none">
+                        <a href="/" class="navbar-brand d-block d-lg-none">
                             <h1 class="m-0 text-primary">Kost KITA</h1>
                         </a>
                         <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -81,20 +81,20 @@
                         </button>
                         <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                             <div class="navbar-nav mr-auto py-0">
-                                <a href="index.html" class="nav-item nav-link active">Home</a>
+                                <a href="/" class="nav-item nav-link active">Home</a>
                                 <!-- <a href="about.html" class="nav-item nav-link">About Us</a> -->
                                 <!-- <a href="service.html" class="nav-item nav-link">Services</a> -->
                                 <!-- <a href="room.html" class="nav-item nav-link">Rooms</a> -->
                                 <div class="nav-item dropdown">
                                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Room</a>
                                     <div class="dropdown-menu rounded-0 m-0">
-                                        <a href="roomcewe.html" class="dropdown-item">Perempuan</a>
-                                        <a href="roomcowo.html" class="dropdown-item">Laki - laki</a>
+                                        <a href="/roomcewe" class="dropdown-item">Perempuan</a>
+                                        <a href="/roomcowo" class="dropdown-item">Laki - laki</a>
                                         <!-- <a href="testimonial.html" class="dropdown-item">Testimonial</a> -->
                                     </div>
                                 </div>
-                                <a href="about.html" class="nav-item nav-link">About Us</a>
-                                <a href="contact.html" class="nav-item nav-link">Contact</a>
+                                <a href="/about" class="nav-item nav-link">About Us</a>
+                                <a href="/contact" class="nav-item nav-link">Contact</a>
                             </div>
                         </div>
                     </nav>
