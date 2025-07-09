aa6ed1fa3149d22f7d55c62a9deb9456c55312c2
Narendra238 <masnarendro123@gmail.com>
2025-07-09 19:39:51 +0700

    Tambah Navbar

--- resources/views/profileanak.blade.php
+++ resources/views/profileanak.blade.php
@@ -6,7 +6,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-<div class="container mt-5">
+<div class="container mt-5 mb-5"><!-- tambahkan mb-5 agar ada jarak bawah -->
     <div class="card">
         <div class="card-body position-relative">
             <h5 class="text-primary fw-bold d-inline-block">PROFIL ANAK KOST</h5>
@@ -27,6 +27,45 @@
         </div>
     </div>
 </div>
+    <!-- Footer Start -->
+        <div class="container-fluid bg-dark text-light footer wow fadeIn pt-5" data-wow-delay="0.1s" style="position:relative; z-index:1;">
+            <div class="container pb-5">
+                <div class="row g-5">
+                    <div class="col-md-6 col-lg-4">
+                        <div class="bg-primary rounded p-4">
+                            <a href="/"><h1 class="text-white mb-3">Kost KITA</h1></a>
+                           
+                        </div><br><br>
+                            <p>Solusi Hunian Nyaman dan Praktis</p>
+                            <p>Proses sewa mudah, tanpa ribet. <br>Lingkungan bersih dan aman. <br>Tanpa drama, langsung tinggal.</p>
+                            <p>Kost rasa rumah sendiri? kostKITA aja!</p>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <h6 class="section-title text-start text-primary text-uppercase mb-4">Contact</h6>
+                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>RT 18 RW 6, Desa Demangan, Kecamatan Godokusuman, Kota Yogyakarta, Daerah Istimewa Yogyakarta</p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a href="https://wa.me/62895417305050" target = _blank>+62 895-4173-05050</a></p>
+                        <p class="mb-2"><i class="fa fa-envelope me-3"></i>kostKITA@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                            &copy; <a class="border-bottom" href="#">Kost KITA</a>, All Right Reserved, 2025 
+                            Designed By <a class="border-bottom" href="#">Ultra Tech Company</a>
+                            <br>Distributed By: <a class="border-bottom" href="#">Ultra Tech Comp.</a>
+                        </div>
+                        <div class="col-md-6 text-center text-md-end">
+                            <div class="footer-menu">
+                                <a href="/">Home</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
